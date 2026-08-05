499c0667bcd9316eef800df5296259f069f99380
soligso-cpu <soligso@whs.school.nz>
2026-08-05 12:17:49 +1200

    Continued lore drops

--- chess-ii--the-final-dlc/Scripts/board.gd
+++ chess-ii--the-final-dlc/Scripts/board.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+var hover = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -7,8 +7,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("Click") and hover == true and Globals.lore_won == true:
+		$LoreOrganizer/Label.visible = true
 
 
 func _on_button_button_up() -> void:
 	Globals.black_turns += 1
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	hover = true
+	print(hover)
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	hover = false
+	print(hover)
