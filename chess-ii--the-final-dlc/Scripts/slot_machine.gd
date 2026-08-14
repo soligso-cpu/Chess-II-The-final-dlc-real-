07e625745e289c9b37c006da5b2c503830e91846
soligso-cpu <soligso@whs.school.nz>
2026-08-14 15:17:28 +1200

    Slost

--- chess-ii--the-final-dlc/Scripts/slot_machine.gd
+++ chess-ii--the-final-dlc/Scripts/slot_machine.gd
@@ -13,41 +13,60 @@ var slot_full_1 = false
 var slot_full_2
 var slot_full_3
 var slot
+var spinning = false
+var slot1
+var slot2
+var slot3
+var line1
+var line2
+var line3
+var top1
+var top2
+var top3
+var purge = false
+var count = 0
+var can_gamble = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	spawn_slot($LineMarkers/Line1)
-	spawn_slot($LineMarkers/Line2)
-	spawn_slot($LineMarkers/Line3)
-	$SpawnTimer.start()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	for body in $Area2D2.get_overlapping_bodies():
+		if purge == true:
+			body.queue_free()
+			count -= 1
+		
+			
+	print("count is,", count)
 		
 func spawn_slot(slot):
-	
+
 		
 	var slot_randi = randi_range(slot_1.SLOT_1_7, slot_1.SLOT_1_JACKPOT)
 	if slot_randi == 0:
 		var slot_7 = SLOT_7_SCENE.instantiate()
 		$SpawnedSlots.add_child(slot_7)
 		slot_7.global_position = slot.global_position
+		slot_7.add_to_group("Slot7Group")
 		
 	if slot_randi == 1:
 		var slot_bust = SLOT_BUST_SCENE.instantiate()
-		$SpawnedSlots.add_child(slot_bust)
+		$SpawnedSlots2.add_child(slot_bust)
 		slot_bust.global_position = slot.global_position
 		
 	if slot_randi == 2:
 		var slot_double = SLOT_DOUBLE_SCENE.instantiate()
-		$SpawnedSlots.add_child(slot_double)
+		$SpawnedSlots3.add_child(slot_double)
 		slot_double.global_position = slot.global_position
+		
 		
 	if slot_randi == 3:
 		var slot_jackpot = SLOT_JACKPOT_SCENE.instantiate()
-		$SpawnedSlots.add_child(slot_jackpot)
+		$SpawnedSlots4.add_child(slot_jackpot)
 		slot_jackpot.global_position = slot.global_position
+		
 	
 
 
@@ -57,6 +76,7 @@ func spawn_slot(slot):
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	body.queue_free()
+	count -= 1
 
 
 func _on_spawn_timer_timeout() -> void:
@@ -86,3 +106,165 @@ func _on_spawn_timer_timeout() -> void:
 		#slot_jackpot.global_position = $LineMarkers/Line1.global_position
 	#
 	#print(slot_randi)
+
+
+func _on_button_button_up() -> void:
+	if spinning == false and can_gamble == true:
+		purge = false
+		
+		spawn_slot($LineMarkers/Line1)
+		spawn_slot($LineMarkers/Line2)
+		spawn_slot($LineMarkers/Line3)
+		$SpawnTimer.start()
+		$StaticBody2D/CollisionShape2D.disabled = true
+		spinning = true
+		
+	elif spinning == true and can_gamble == true:
+		
+		print("next step, gud")
+		$StaticBody2D/CollisionShape2D.disabled = false
+		$SpawnTimer.stop()
+		spawn_slot($LineMarkers/Line1)
+		spawn_slot($LineMarkers/Line2)
+		spawn_slot($LineMarkers/Line3)
+		print($SlotDetection/Line3.get_overlapping_bodies() )
+		if count < 9:
+			$SpawnTimer.start()
+
+		spinning = false
+		$ProcessTimer.start()
+		can_gamble = false
+
+
+	
+
+
+func _on_slot_3_body_entered(body: Node2D) -> void:
+	if body.get_parent() == $SpawnedSlots:
+		slot1 = 1
+	elif body.get_parent() == $SpawnedSlots2:
+		slot1 = 2
+	elif body.get_parent() == $SpawnedSlots3:
+		slot1 = 3
+	elif body.get_parent() == $SpawnedSlots4:
+		slot1 = 4
+	print(slot1)
+
+func _on_slot_2_body_entered(body: Node2D) -> void:
+	if body.get_parent() == $SpawnedSlots:
+		slot2 = 1
+	elif body.get_parent() == $SpawnedSlots2:
+		slot2 = 2
+	elif body.get_parent() == $SpawnedSlots3:
+		slot2 = 3
+	elif body.get_parent() == $SpawnedSlots4:
+		slot2 = 4
+	print(slot2)
+
+
+func _on_slot_1_body_entered(body: Node2D) -> void:
+	if body.get_parent() == $SpawnedSlots:
+		slot3 = 1
+	elif body.get_parent() == $SpawnedSlots2:
+		slot3 = 2
+	elif body.get_parent() == $SpawnedSlots3:
+		slot3 = 3
+	elif body.get_parent() == $SpawnedSlots4:
+		slot3 = 4
+	print(slot3)
+
+func _on_process_timer_timeout() -> void:
+	print(slot1,slot2,slot3)
+	if slot1 == slot2 and slot2 == slot3:
+		
+		print("WOW IT WORKS")
+		
+	if line1 == line2 and line2 == line3:
+		print("WOW IT WORKS PT 2")
+		
+	if top1 == top2 and top2 == top3:
+		print("WOW IT WORKS 3")
+	$PurgeTimer.start()
+		
+	
+
+
+func _on_purge_timer_timeout() -> void:
+	if purge == false:
+		purge = true
+		$PurgeTimer.start()
+	elif purge == true:
+		purge = false
+		can_gamble = true
+
+
+func _on_line_1_body_entered(body: Node2D) -> void:
+	if body.get_parent() == $SpawnedSlots:
+		line1 = 1
+	elif body.get_parent() == $SpawnedSlots2:
+		line1 = 2
+	elif body.get_parent() == $SpawnedSlots3:
+		line1 = 3
+	elif body.get_parent() == $SpawnedSlots4:
+		line1 = 4
+	print(line1)
+
+
+func _on_line_2_body_entered(body: Node2D) -> void:
+	if body.get_parent() == $SpawnedSlots:
+		line2 = 1
+	elif body.get_parent() == $SpawnedSlots2:
+		line2 = 2
+	elif body.get_parent() == $SpawnedSlots3:
+		line2 = 3
+	elif body.get_parent() == $SpawnedSlots4:
+		line2 = 4
+	print(line2)
+
+
+func _on_line_3_body_entered(body: Node2D) -> void:
+	if body.get_parent() == $SpawnedSlots:
+		line3 = 1
+	elif body.get_parent() == $SpawnedSlots2:
+		line3 = 2
+	elif body.get_parent() == $SpawnedSlots3:
+		line3 = 3
+	elif body.get_parent() == $SpawnedSlots4:
+		line3 = 4
+	print(line3)
+
+
+func _on_top_1_body_entered(body: Node2D) -> void:
+	if body.get_parent() == $SpawnedSlots:
+		top1 = 1
+	elif body.get_parent() == $SpawnedSlots2:
+		top1 = 2
+	elif body.get_parent() == $SpawnedSlots3:
+		top1 = 3
+	elif body.get_parent() == $SpawnedSlots4:
+		top1 = 4
+	print(top1)
+
+
+func _on_top_2_body_entered(body: Node2D) -> void:
+	if body.get_parent() == $SpawnedSlots:
+		top2 = 1
+	elif body.get_parent() == $SpawnedSlots2:
+		top2 = 2
+	elif body.get_parent() == $SpawnedSlots3:
+		top2 = 3
+	elif body.get_parent() == $SpawnedSlots4:
+		top2 = 4
+	print(top2)
+
+
+func _on_top_3_body_entered(body: Node2D) -> void:
+	if body.get_parent() == $SpawnedSlots:
+		top3 = 1
+	elif body.get_parent() == $SpawnedSlots2:
+		top3 = 2
+	elif body.get_parent() == $SpawnedSlots3:
+		top3 = 3
+	elif body.get_parent() == $SpawnedSlots4:
+		top3 = 4
+	print(top3)
