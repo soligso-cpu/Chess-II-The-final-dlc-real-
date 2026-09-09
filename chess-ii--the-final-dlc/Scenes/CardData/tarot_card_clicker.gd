b0cd6ad1ccd30c744397fb370d9c0d5a33212efb
peanutallergy17 <kunzemi@whs.school.nz>
2026-09-09 12:20:21 +1200

    more cards

--- chess-ii--the-final-dlc/Scenes/CardData/tarot_card_clicker.gd
+++ chess-ii--the-final-dlc/Scenes/CardData/tarot_card_clicker.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
 signal tarot_card_action(left: bool)
+signal tarot_card_release(left: bool)
 
 func _input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("Click"):
 		tarot_card_action.emit(true)
 	if event.is_action_pressed("ClickR"):
 		tarot_card_action.emit(false)
+	if event.is_action_released("Click"):
+		tarot_card_release.emit(true)
 	pass
